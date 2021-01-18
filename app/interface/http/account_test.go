28a141c71147f1cd8c8b01d426e49f985453eba3
VLaroye <laroye.vincent@gmail.com>
2021-01-18 21:09:37 +0100

    Added one check to http ListAccounts endpoint's test

--- app/interface/http/account_test.go
+++ app/interface/http/account_test.go
@@ -1,14 +1,23 @@
 package http
 
 import (
-	mock_usecase "github.com/VLaroye/gasso-back/app/usecase"
+	"encoding/json"
+	"github.com/VLaroye/gasso-back/app/domain/model"
+	mockUsecase "github.com/VLaroye/gasso-back/app/usecase"
 	"github.com/golang/mock/gomock"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+var mockAccount *model.Account = model.NewAccount("123456", "example1")
+
 func TestListAccounts(t *testing.T) {
+	type listAccountsResponse struct {
+		Accounts []*Account
+	}
+	var response listAccountsResponse
+
 	req, err := http.NewRequest("GET", "/accounts", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -17,8 +26,10 @@ func TestListAccounts(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	ctrl := gomock.NewController(t)
-	accountUsecase := mock_usecase.NewMockAccountUsecase(ctrl)
-	accountUsecase.EXPECT().ListAccounts().Return(nil, nil)
+	accountUsecase := mockUsecase.NewMockAccountUsecase(ctrl)
+	accountUsecase.EXPECT().ListAccounts().Return([]*model.Account{
+		mockAccount,
+	}, nil)
 
 	accountService := NewAccountService(accountUsecase)
 
@@ -28,5 +39,12 @@ func TestListAccounts(t *testing.T) {
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status: got %v want %v",
 			status, http.StatusOK)
+	}
+
+	json.NewDecoder(rr.Body).Decode(&response)
+
+	if len(response.Accounts) != 1 {
+		t.Errorf("wrong number of accounts: got %v want %v",
+			len(response.Accounts), 1)
 	}
 }
