8af0b2f90cffc5835f7ad14821562b0d77826be1
VLaroye <laroye.vincent@gmail.com>
2021-02-01 19:32:32 +0100

    Add basic docker-compose.yml to start postgres db + adminer / Add env variables to configure db access

--- cmd/gasso/main.go
+++ cmd/gasso/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"github.com/VLaroye/gasso-back/app/interface/db"
 	"github.com/VLaroye/gasso-back/app/interface/http"
-	"gorm.io/driver/sqlite"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func main() {
-	port := 5000
-	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	http.Start(port, database)
+	// TODO: Handle migrations on a subcommand ? Here only for dev (?)
+	if err := database.AutoMigrate(&db.User{}, &db.Account{}, &db.Invoice{}); err != nil {
+		panic(err)
+	}
+
+	http.Start(5000, database)
 }
--- go.mod
+++ go.mod
@@ -5,12 +5,17 @@ go 1.15
 require (
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/mux v1.8.0
+	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/pkg/errors v0.9.1 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/tools v0.0.0-20200103221440-774c71fcf114 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/gorm v1.20.8
+	gorm.io/driver/postgres v1.0.7
+	gorm.io/gorm v1.20.12
 )
