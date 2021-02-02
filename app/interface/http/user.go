b38d4b9f541002e5cfea68977d07a9a0c3c68f0d
VLaroye <laroye.vincent@gmail.com>
2021-02-02 11:52:51 +0100

    Add JWT token generation on user's login

--- app/interface/http/user.go
+++ app/interface/http/user.go
@@ -3,7 +3,11 @@ package http
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/VLaroye/gasso-back/app/domain/model"
 	"github.com/VLaroye/gasso-back/app/usecase"
@@ -19,6 +23,11 @@ func RegisterUserHandlers(router *mux.Router, service *userService) {
 type User struct {
 	ID    string `json:"id"`
 	Email string `json:"email"`
+}
+
+type JWTClaims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
 }
 
 type userService struct {
@@ -39,29 +48,55 @@ func toUsers(users []*model.User) []*User {
 }
 
 func (u *userService) Login(w http.ResponseWriter, r *http.Request) {
-	type registerUserRequest struct {
-		Email string
+	type signInRequest struct {
+		Email    string
 		Password string
 	}
 
-	var request registerUserRequest
+	var request signInRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		respondError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	if err := u.userUsecase.Login(request.Email, request.Password); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		respondError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	fmt.Println("Logged in")
+	jwtExpiracyDelay, err := strconv.Atoi(os.Getenv("JWT_EXPIRATION_DELAY"))
+	if err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	tokenExpirationTime := time.Now().Add(time.Duration(jwtExpiracyDelay) * time.Minute)
+
+	claims := &JWTClaims{
+		Email: request.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: tokenExpirationTime.Unix(),
+		},
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := jwtToken.SignedString([]byte(os.Getenv("JWT_KEY")))
+	if err != nil {
+		fmt.Println(err, os.Getenv("JWT_KEY"))
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: tokenExpirationTime,
+	})
 
 	respondJSON(w, nil)
 }
 
-func (u *userService) ListUsers(w http.ResponseWriter, r *http.Request) {
+func (u *userService) ListUsers(w http.ResponseWriter, _ *http.Request) {
 	type listUsersResponseType struct {
 		Error error   `json:"error"`
 		Users []*User `json:"users"`
@@ -82,7 +117,7 @@ func (u *userService) ListUsers(w http.ResponseWriter, r *http.Request) {
 
 func (u *userService) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	type registerUserRequest struct {
-		Email string
+		Email    string
 		Password string
 	}
 
